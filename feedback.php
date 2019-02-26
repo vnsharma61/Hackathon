50175441684e76626b67cff538721c962dbc52db
Nimit Sharma <vnsharma61@gmail.com>
2019-02-26 21:51:57 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -83,9 +83,35 @@ if(isset($_POST["submit"]))
 }
 
 ?>
+
+
+
+
 <!DOCTYPE html>
 <html>
  <head>
+
+
+
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+     <!-- Bootstrap CSS -->
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+       <!--icon for footer-->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+
+       <!--style for this webbapp//css file-->
+       <link rel="stylesheet" type="text/css" href="css/style.css">
+
+       <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Roboto|Roboto+Condensed" rel="stylesheet">
+
+     <title>Neophytes | Online Assessment</title>
+
+
+
   <title>FEEDBACK FORM</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
